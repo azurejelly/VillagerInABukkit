4060cedf2756290a59baa2563a22fe7dd16d4fa4
doom <61824552+JustDoom@users.noreply.github.com>
2025-04-06 13:57:03 +1000

    Add villager data as lore

--- src/main/java/com/imjustdoom/villagerinabucket/Main.java
+++ src/main/java/com/imjustdoom/villagerinabucket/Main.java
@@ -1,12 +1,16 @@
 package com.imjustdoom.villagerinabucket;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -15,6 +19,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends JavaPlugin implements Listener {
     public NamespacedKey key = new NamespacedKey(this, "villager_data");
@@ -33,6 +40,22 @@ public class Main extends JavaPlugin implements Listener {
         return dataContainer.has(this.key) && dataContainer.get(this.key, PersistentDataType.BYTE_ARRAY) != null;
     }
 
+    public void createVillagerBucket(ItemStack itemStack, Villager villager) {
+        itemStack.editMeta(meta -> {
+            meta.customName(Component.text("Villager In A Bucket"));
+            meta.getPersistentDataContainer().set(this.key, PersistentDataType.BYTE_ARRAY, Bukkit.getUnsafe().serializeEntity(villager));
+            meta.setMaxStackSize(1);
+            List<Component> lore = new ArrayList<>();
+            lore.add(Component.text("Level: " + villager.getVillagerLevel(), TextColor.color(Color.GRAY.asRGB()), TextDecoration.ITALIC));
+            lore.add(Component.text("Region: " + villager.getVillagerType().getKey().getKey().toUpperCase(), TextColor.color(Color.GRAY.asRGB()), TextDecoration.ITALIC));
+            lore.add(Component.text("Profession: " + villager.getProfession(), TextColor.color(Color.GRAY.asRGB()), TextDecoration.ITALIC));
+            if (!villager.isAdult()) {
+                lore.add(Component.text("Baby", TextColor.color(Color.GRAY.asRGB()), TextDecoration.ITALIC));
+            }
+            meta.lore(lore);
+        });
+    }
+
     @EventHandler
     public void villagerInteract(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
@@ -43,19 +66,11 @@ public class Main extends JavaPlugin implements Listener {
         }
         if (itemStack.getAmount() > 1) {
             ItemStack newStack = new ItemStack(Material.BUCKET);
-            newStack.editMeta(meta -> {
-                meta.customName(Component.text("Villager In A Bucket"));
-                meta.getPersistentDataContainer().set(this.key, PersistentDataType.BYTE_ARRAY, Bukkit.getUnsafe().serializeEntity(clicked));
-                meta.setMaxStackSize(1);
-            });
+            createVillagerBucket(newStack, (Villager) clicked);
             itemStack.setAmount(itemStack.getAmount() - 1);
             player.getInventory().addItem(newStack);
         } else {
-            itemStack.editMeta(meta -> {
-                meta.customName(Component.text("Villager In A Bucket"));
-                meta.getPersistentDataContainer().set(this.key, PersistentDataType.BYTE_ARRAY, Bukkit.getUnsafe().serializeEntity(clicked));
-                meta.setMaxStackSize(1);
-            });
+            createVillagerBucket(itemStack, (Villager) clicked);
         }
         clicked.remove();
         event.setCancelled(true);
@@ -76,6 +91,9 @@ public class Main extends JavaPlugin implements Listener {
             meta.customName(null);
             meta.getPersistentDataContainer().remove(this.key);
             meta.setMaxStackSize(null);
+            if (meta.hasLore()) {
+                meta.lore(null);
+            }
         });
         event.setCancelled(true);
     }
